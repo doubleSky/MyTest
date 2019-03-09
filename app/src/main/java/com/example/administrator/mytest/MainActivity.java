333e5caf2d5b73427c691cd95df1a237607545a5
doubleSky <744500803@qq.com>
2019-03-09 11:17:26 +0800

    初始版本

---
+++ app/src/main/java/com/example/administrator/mytest/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.administrator.mytest;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        startActivity(new Intent(this, Main2Activity.class));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 11111);
+        }else {
+            Log.i("1111", "onResume: 1111");
+        }
+    }
+}
